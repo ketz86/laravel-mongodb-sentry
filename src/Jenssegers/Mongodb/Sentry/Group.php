83083f8016902765badf3540cd0b64a8351239ca
Davide Luchetta <luchettad@gmail.com>
2019-12-21 16:14:42 +0100

    Update Group.php

--- src/Jenssegers/Mongodb/Sentry/Group.php
+++ src/Jenssegers/Mongodb/Sentry/Group.php
@@ -21,7 +21,7 @@
 use Cartalyst\Sentry\Groups\NameRequiredException;
 use Cartalyst\Sentry\Groups\GroupExistsException;
 use Cartalyst\Sentry\Groups\GroupInterface;
-use Jenssegers\Mongodb\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class Group extends Model implements GroupInterface {
 
